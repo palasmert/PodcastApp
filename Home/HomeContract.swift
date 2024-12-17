543a9e62f7ca849741ecc06abdbbb2834459a05c
palasmert <mertpalas39@gmail.com>
2024-12-18 02:04:09 +0300

    add VIPER template to project

---
+++ Home/HomeContract.swift
@@ -0,0 +1,39 @@
+//
+//  HomeContract.swift
+//  PodcastApp
+//
+//  Created by Mert Palas on 18.12.2024.
+//  
+//
+
+import Foundation
+
+
+// MARK: View Output (Presenter -> View)
+protocol PresenterToViewHomeProtocol {
+   
+}
+
+
+// MARK: View Input (View -> Presenter)
+protocol ViewToPresenterHomeProtocol {
+}
+
+
+// MARK: Interactor Input (Presenter -> Interactor)
+protocol PresenterToInteractorHomeProtocol {
+    
+    var presenter: InteractorToPresenterHomeProtocol? { get set }
+}
+
+
+// MARK: Interactor Output (Interactor -> Presenter)
+protocol InteractorToPresenterHomeProtocol {
+    
+}
+
+
+// MARK: Router Input (Presenter -> Router)
+protocol PresenterToRouterHomeProtocol {
+    
+}
---
+++ Home/HomeInteractor.swift
@@ -0,0 +1,15 @@
+//
+//  HomeInteractor.swift
+//  PodcastApp
+//
+//  Created by Mert Palas on 18.12.2024.
+//  
+//
+
+import Foundation
+
+final class HomeInteractor: PresenterToInteractorHomeProtocol {
+
+    // MARK: Properties
+    var presenter: InteractorToPresenterHomeProtocol?
+}
