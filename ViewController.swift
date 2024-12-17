2187e2a2dd968004343e49de4cbe7342af627100
palasmert <mertpalas39@gmail.com>
2024-12-18 01:26:53 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PodcastApp
+//
+//  Created by Mert Palas on 10.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+    }
+}
+
+#Preview {
+    ViewController()
+
+}
